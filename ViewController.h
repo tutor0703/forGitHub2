a52cbdb2d949469dec31a495ea77ca8e6a41a4b0
tutor0703 <tutor0703@gmail.com>
2017-03-02 12:01:32 +0200

    Add files via upload

---
+++ ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  forGitHub2
+//
+//  Created by tutor0703 on 02.03.17.
+//  Copyright © 2017 tutor0703. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  forGitHub2
+//
+//  Created by tutor0703 on 02.03.17.
+//  Copyright © 2017 tutor0703. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
